c74de3824a4014655f0c78de428b596f1a37743d
Saksham <saksham.gairola28@gmail.com>
2023-01-29 16:20:55 +0530

    Initial commit

---
+++ src/main/java/com/greatlearning/studentMgmt/SakshamBedLabRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.greatlearning.studentMgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakshamBedLabRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakshamBedLabRestApiApplication.class, args);
+	}
+
+}
